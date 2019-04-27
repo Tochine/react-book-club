2ced95324f4659fb0ceb2973d5dff0ab467016a9
Tochine <toshineluv99@yahoo.com>
2019-04-27 14:07:48 +0100

    adding how it works section on the home page

--- src/frontend/Home.js
+++ src/frontend/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./Layouts.css";
-import { Col, Row } from "antd";
+import "./Home.css";
+import { Col, Row, Layout } from "antd";
 import { Link } from "@reach/router";
 import banner from "../images/book1.jpg";
+import book1 from "../images/book1.jpg";
 
 class Home extends Component {
   render() {
+    const { Content } = Layout;
+
     return (
       <div>
         <section style={{ minHeight: "600px" }}>
@@ -34,6 +38,47 @@ class Home extends Component {
             </Link>
           </Col>
         </section>
+        <Row>
+          <Layout>
+            <h4 className="front-title">HOW IT WORKS</h4>
+            <section>
+              <Content>
+                <Row style={{ height: "290px" }}>
+                  <Col span={8} style={{ height: "100%", textAlign: "center" }}>
+                    <img
+                      src={banner}
+                      style={{ maxWidth: "auto", height: "200px" }}
+                      alt="book"
+                    />
+                    <p className="front-description">
+                      Schedule and vist one of our centers
+                    </p>
+                  </Col>
+                  <Col span={8} style={{ height: "100%", textAlign: "center" }}>
+                    <img
+                      src={banner}
+                      style={{ maxWidth: "auto", height: "200px" }}
+                      alt="book"
+                    />
+                    <p className="front-description">
+                      Schedule and vist one of our centers
+                    </p>
+                  </Col>
+                  <Col span={8} style={{ height: "100%", textAlign: "center" }}>
+                    <img
+                      src={banner}
+                      style={{ maxWidth: "auto", height: "200px" }}
+                      alt="book"
+                    />
+                    <p className="front-description">
+                      Schedule and vist one of our centers
+                    </p>
+                  </Col>
+                </Row>
+              </Content>
+            </section>
+          </Layout>
+        </Row>
       </div>
     );
   }
